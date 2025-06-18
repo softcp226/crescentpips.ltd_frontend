da57e5dcd089f611eebcfed7f96889dfef581731
softcp226 <softcp226@gmail.com>
2025-06-18 01:33:42 -0700

    changes

--- javascript/withdrawal.js
+++ javascript/withdrawal.js
@@ -78,8 +78,8 @@ document.querySelector("#submit").onclick = () => {
     token: checkCookie("token"),
     user: checkCookie("user"),
     withdrawal_amount: withdrawal_amount.value,
-    account_name: withdrawal_method.value,
-    account_number: wallet.value,
+   transaction_bank: withdrawal_method.value,
+     account_name: wallet.value,
   });
 };
 
